63ec3463ff9b81de7a0ef43d1625daeab37f7402
ltrajkovic92 <lazar.trajkovic92@gmail.com>
2023-10-01 19:29:49 +0200

    Add exercises: selenium actions

---
+++ src/main/java/p29_09_2023/UvodUAction.java
@@ -0,0 +1,33 @@
+package p29_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class UvodUAction {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        driver.get("https://docs.katalon.com/");
+
+        WebElement footer = driver.findElement(By.tagName("footer"));
+
+//        Actions actions = new Actions(driver);
+//        actions.scrollToElement(footer);
+//        actions.perform();
+
+        new Actions(driver).scrollToElement(footer).perform();
+    }
+}
