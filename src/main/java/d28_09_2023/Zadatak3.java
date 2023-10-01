a23c50a2a055ae3083787780016af8320b38b4ca
ltrajkovic92 <lazar.trajkovic92@gmail.com>
2023-10-01 18:31:40 +0200

    Fix issues in Zadatak3 test

---
+++ src/main/java/d28_09_2023/Zadatak3.java
@@ -0,0 +1,36 @@
+package d28_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Zadatak3 {
+    public static void main(String[] args) throws InterruptedException{
+
+//        3. Zadatak
+//        Za zadatak iz foldera za domaci izdebagirati i ispraviti da radi.
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("http://seleniumdemo.com/?product=git-basics");
+
+        WebElement addToCartBtn = driver.findElement(By.name("add-to-cart"));
+
+        addToCartBtn.click();
+        Thread.sleep(1000); // cekaj da se osvezi stranica
+
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".nav__woocart .count"), "1"));
+
+        System.out.println("KRAJ");
+
+        driver.quit();
+    }
+}
